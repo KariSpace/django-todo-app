239ccd7814af5fdf108641f600f79b99f23da337
KariSpace <kpivtorak55@gmail.com>
2022-08-15 01:33:31 +0300

    Add TaskDelete

--- todo/todo_list/urls.py
+++ todo/todo_list/urls.py
@@ -1,9 +1,11 @@
 from django.urls import URLPattern, path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('details/<int:pk>/', TaskDetail.as_view(), name='details'),
     path('create/', TaskCreate.as_view(), name='create'),
-    path('update/<int:pk>/', TaskUpdate.as_view(), name='update')
+    path('update/<int:pk>/', TaskUpdate.as_view(), name='update'),
+    path('delete/<int:pk>/', TaskDelete.as_view(), name='delete')
+
 ]
