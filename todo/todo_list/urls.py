b2235579abba109205e7ee0bc605965b1e884b72
KariSpace <kpivtorak55@gmail.com>
2022-08-15 01:24:01 +0300

    Add updateView

--- todo/todo_list/urls.py
+++ todo/todo_list/urls.py
@@ -1,8 +1,9 @@
 from django.urls import URLPattern, path
-from .views import TaskList, TaskDetail, TaskCreate
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('details/<int:pk>/', TaskDetail.as_view(), name='details'),
-    path('create/', TaskCreate.as_view(), name='create')
+    path('create/', TaskCreate.as_view(), name='create'),
+    path('update/<int:pk>/', TaskUpdate.as_view(), name='update')
 ]
