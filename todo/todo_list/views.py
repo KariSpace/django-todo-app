f2db6a1405e7d9d1495990f4571effd890ec4a40
KariSpace <kpivtorak55@gmail.com>
2022-08-15 21:35:48 +0300

    Added back button

--- todo/todo_list/views.py
+++ todo/todo_list/views.py
@@ -1,12 +1,15 @@
-from attr import field
+# from attr import field
 from django.shortcuts import render
 # from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+# from django.contrib.auth.views import LoginView
 
 from .models import Task
+
+
 
 # Create your views here.
 class TaskList(ListView): 
